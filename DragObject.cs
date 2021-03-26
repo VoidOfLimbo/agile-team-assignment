34eefb78d1a1d3b5f65a22acfafa8a533854059b
Fairooz-J <72198058+Fairooz-J@users.noreply.github.com>
2021-03-26 15:29:32 +0000

    Create DragObject.cs
    Dragging object with mouse.

---
+++ DragObject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour
+{
+
+    Vector3 dist;
+    float posX;
+    float posY;
+    public ConstantForce gravity;
+
+    void OnMouseDown()
+    {
+        dist = Camera.main.WorldToScreenPoint(transform.position);
+        posX = Input.mousePosition.x - dist.x;
+        posY = Input.mousePosition.y - dist.y;
+
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curPos =
+         new Vector3(Input.mousePosition.x - posX,
+                     Input.mousePosition.y - posY, dist.z);
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(curPos);
+        transform.position = worldPos;
+    }
+
+    void OnMouseUp()
+    {
+        gravity = gameObject.AddComponent<ConstantForce>();
+        gravity.force = new Vector3(0.0f, -9.81f * Time.deltaTime, 0.0f);
+    }
+
+}
